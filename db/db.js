d39e75a75532e26c1310986bc6b6a85f6d70a636
Patrick Alexandre <palexandre822@gmail.com>
2023-09-14 11:15:03 -0700

    conversion of react native to react project

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: process.env.EXPO_PUBLIC_DB_HOST,
-  port: process.env.EXPO_PUBLIC_DB_PORT,
-  database: process.env.EXPO_PUBLIC_DB_DATABASE,
-  user: process.env.EXPO_PUBLIC_DB_USER,
-  password: process.env.EXPO_PUBLIC_DB_PASSWORD
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
 module.exports = client;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 const router = require('./routes');
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 app.use('/', router);
 
