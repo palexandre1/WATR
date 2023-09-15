27870d095b73bf56aa5f3d9b9d1f33fd626f8438
Patrick Alexandre <palexandre822@gmail.com>
2023-09-14 19:59:23 -0700

    rendered map

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const router = express.Router();
 const controller = require('./controllers');
 
+router.get('/key', function(req, res) {
+  res.status(200).send(process.env.GOOGLE_API_KEY)
+})
 router.get('/courts', controller.getCourts);
 router.put('/courts/:id', controller.updatePlayerCount);
 
